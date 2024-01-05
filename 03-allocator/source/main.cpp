343e59922d6da4e66284c0ce453fae5c69e9cfc5
Alexey Tyurin <johndoe@example.com>
2024-01-05 19:37:14 +0300

    [03-allocator] upd main: making own (stolen) MyList

--- 03-allocator/source/main.cpp
+++ 03-allocator/source/main.cpp
@@ -1,10 +1,10 @@
 
 #include <assert.h>
-
 #include <iostream>
 #include <map>
 
-static const int numberElems = 10;
+// #define DEBUG_ALLOCATOR
+static const int NUMBER_ELEMS = 10;
 
 template <typename T, typename U>
 std::ostream& operator<<(std::ostream& stream, const std::pair<T, U>& pair) {
@@ -36,9 +36,11 @@ class Allocator {
   };
 
   T* allocate(std::size_t n) {
+#ifdef DEBUG_ALLOCATOR
     std::cout << "[Allocator::allocate] size = " << size << ", n = " << n
               << ", m_allocated_size + n = " << m_allocated_size + n
               << std::endl;
+#endif
 
     assert(size - m_allocated_size >= n);
     m_allocated_size += n;
@@ -46,13 +48,11 @@ class Allocator {
   }
   void deallocate(T*, std::size_t n) {
     m_allocated_size -= n;
+#ifdef DEBUG_ALLOCATOR
     std::cout << "[Allocator::deallocate] m_allocated_size = "
               << m_allocated_size + n << ", n = " << n
               << ", new m_allocated_size = " << m_allocated_size << std::endl;
-
-    //    std::size_t from = p - m_data;
-    //    std::cout << "[Allocator::deallocate] *p = " << *p << std::endl;
-    //    ::operator delete(p);
+#endif
   }
 
   template <class Up, class... Args>
@@ -69,6 +69,57 @@ class Allocator {
 
 //============================[allocator END]
 
+//============================[myList BEGIN]
+
+template <typename T, typename Allocator = std::allocator<T>>
+class MyList {
+  struct Node {
+    Node(T value, Node* next) : value(std::move(value)), next(next) {}
+
+    T value;
+    Node* next;
+  };
+
+ public:
+  MyList() = default;
+
+  ~MyList() {
+    assert((size == 0 && head == nullptr) || (size != 0 && head != nullptr));
+    while (head != nullptr) {
+      Node* ptr = head;
+      head = head->next;
+      nodeAlloc.destroy(ptr);
+      nodeAlloc.deallocate(ptr, 1);
+      --size;
+    }
+  }
+
+  void push_front(const T& value) {
+    Node* newNode = nodeAlloc.allocate(1);
+    nodeAlloc.construct(newNode, std::move(value), head);
+    head = newNode;
+    ++size;
+  }
+
+  void print_container() {
+    assert((size == 0 && head == nullptr) || (size != 0 && head != nullptr));
+    Node* local_head = head;
+    while (local_head != nullptr) {
+      Node* ptr = local_head;
+      local_head = local_head->next;
+      std::cout << ptr->value << '\n';
+    }
+  }
+
+ private:
+  using NodeAllocator = typename Allocator::template rebind<Node>::other;
+  Node* head = nullptr;
+  NodeAllocator nodeAlloc;
+  std::size_t size = 0;
+};
+
+//============================[myList END]
+
 int factorial(int number) {
   int ans = 1;
   for (int i = 2; i <= number; ++i) {
@@ -79,8 +130,15 @@ int factorial(int number) {
 
 template <typename Allocator>
 void fill_map(std::map<int, int, std::less<int>, Allocator>& map) {
-  for (int i = 0; i < numberElems; ++i) {
+  for (int i = 0; i < NUMBER_ELEMS; ++i) {
     map.emplace(i, factorial(i));
+  }
+}
+
+template <typename Allocator>
+void fill_list(MyList<int, Allocator>& list) {
+  for (int i = NUMBER_ELEMS - 1; i >= 0; --i) {
+    list.push_front(i);
   }
 }
 
@@ -91,31 +149,36 @@ void print_container(const Container& container) {
   }
 }
 
-// void print_map(const std::map<int, int> m_map) {
-//   for (const auto& elem : m_map) {
-//     std::cout << elem.first << ", " << elem.second << std::endl;
-//   }
-// }
-
 int main() {
   try {
-    std::cout << "[main] Hello world 3" << std::endl;
-
+    //    std::cout << "[main] Hello world 3" << std::endl;
+    // myAllocator
     std::map<int, int> map_std_alloc;
-    std::map<int, int, std::less<int>, Allocator<std::pair<const int, int>, 10>>
+    std::map<int, int, std::less<int>,
+             Allocator<std::pair<const int, int>, NUMBER_ELEMS>>
         map_my_alloc;
 
     fill_map(map_std_alloc);
     fill_map(map_my_alloc);
-
-    //    map_std_alloc.insert(std::pair<int, int>(100, 110));
-    //    map_my_alloc.insert(std::pair<int, int>(100, 110));
 
     std::cout << "map_std_alloc:" << std::endl;
     print_container(map_std_alloc);
 
     std::cout << "map_my_alloc:" << std::endl;
     print_container(map_my_alloc);
+
+    // myList
+    MyList<int> list_std_alloc;
+    MyList<int, Allocator<int, NUMBER_ELEMS>> list_my_alloc;
+
+    fill_list(list_std_alloc);
+    fill_list(list_my_alloc);
+
+    std::cout << "list_std_alloc:" << std::endl;
+    list_std_alloc.print_container();
+
+    std::cout << "list_my_alloc:" << std::endl;
+    list_my_alloc.print_container();
 
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
